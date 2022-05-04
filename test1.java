cc5c941b5d317d9b50d0e64b9d3f123d87af9bfd
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-05-04 16:28:29 -0500

    Prueba de subida de archivos a rama cgarayb, y prueba de condicionales

---
+++ test1.java
@@ -0,0 +1,9 @@
+public class test1 {
+
+    public static void main(String args[]) {
+        System.out.println("Tu edad es de 17 años");
+        int edad = 17;
+        String variable = (edad >= 18) ? "Eres mayor de 18" : "No eres mayor de edad!";
+        System.out.println(variable);
+    }
+}
